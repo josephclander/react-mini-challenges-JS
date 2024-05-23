3027c40395c83b9430fe0c2777e9209463819fbb
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-23 23:26:41 +0100

    solution to challenge 10

--- src/solutions/Solution10.jsx
+++ src/solutions/Solution10.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+
 const Solution10 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const { data } = response.json();
+        setCountries(data.slice(0, 20));
+        setLoading(false);
+      } catch (error) {
+        setError("Error loading data");
+        setLoading(false);
+      }
+    };
+  }, []);
+  return (
+    <div>
+      {loading && <div>Loading data, please wait</div>}
+      {error && <div>Error loading data</div>}
+      {countries &&
+        countries.map((country) => (
+          <div key={country.ccn3}>{country?.name?.common}</div>
+        ))}
+    </div>
+  );
 };
 
 export default Solution10;
