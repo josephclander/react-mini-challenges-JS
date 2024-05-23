76ea2034b77af6adef1c20d5059c46cf6b59905c
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2024-05-23 23:09:13 +0100

    solution to challenge 10

--- src/solutions/Solution10.jsx
+++ src/solutions/Solution10.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react";
+
 const Solution10 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [myData, setMyData] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const { data } = await response.json();
+        setMyData(data);
+        console.log(myData);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+  }, []);
+  return (
+    <div>
+      {error && <div>Error loading data</div>}
+      {loading && <div>Loading, please wait</div>}
+      <ul>
+        {myData &&
+          myData.map((item) => <li key={item.ccn3}>{item?.name?.common}</li>)}
+      </ul>
+    </div>
+  );
 };
 
 export default Solution10;
